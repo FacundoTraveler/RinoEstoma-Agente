4b4e8fabf6ce4185563e03d744d60adcac711c24
Facundo Traveler <101372370+FacundoTraveler@users.noreply.github.com>
2026-04-25 19:23:21 -0300

    Add custom headers to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/agente',
+  basePath: '/agente',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://www.rinoestoma.com',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://www.rinoestoma.com",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+        ],
+      },
+    ]
+  },
 }
-
 export default nextConfig
